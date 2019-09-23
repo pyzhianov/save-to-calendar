0f0be21ff41f94544facdf4693a4227d4f34943d
Alexey Pyzhianov <pyzhyanovai@sibur.ru>
2019-09-23 20:36:22 +0300

    Updates

---
+++ precache-manifest.d71bdad64a0649d78883eafdeed22a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d027aa56c3667f25dafdb53f793142f2",
+    "url": "/pyzhianov/save-to-calendar/index.html"
+  },
+  {
+    "revision": "cdef995c03fe964ada26",
+    "url": "/pyzhianov/save-to-calendar/static/css/2.1ef35742.chunk.css"
+  },
+  {
+    "revision": "a4bf8e527f2e3ec5cbd6",
+    "url": "/pyzhianov/save-to-calendar/static/css/main.10646d40.chunk.css"
+  },
+  {
+    "revision": "cdef995c03fe964ada26",
+    "url": "/pyzhianov/save-to-calendar/static/js/2.d76424d4.chunk.js"
+  },
+  {
+    "revision": "a4bf8e527f2e3ec5cbd6",
+    "url": "/pyzhianov/save-to-calendar/static/js/main.a3e792b9.chunk.js"
+  },
+  {
+    "revision": "e0f26f1a0673b9f5a816",
+    "url": "/pyzhianov/save-to-calendar/static/js/runtime~main.fb28a2e6.js"
+  }
+]);
